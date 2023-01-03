241452598ef781bf4cf545edc86a2af84d7824a1
KshitizSachan <kshitizsachan422@gmail.com>
2023-01-03 22:14:45 +0530

    Keeper app challenge 1 done

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+    return (
+     <div className="note">
+        <h1>for the title</h1>
+        <p>for the content</p>
+     </div>
+    );
+}
+export default Note;
